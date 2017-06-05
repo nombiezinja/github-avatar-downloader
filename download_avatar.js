3965c8b229a3b292ddabbc81aba1167c27dc6d14
Ti Zhang <tizhang8835@gmail.com>
2017-06-05 23:13:38 +0000

    Adds step 2 - awaiting correction

--- download_avatar.js
+++ download_avatar.js
@@ -2,3 +2,19 @@ var request = require('request');
 
 console.log('Welcome to GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+         .on('error', function(err) {
+          console.log('An error has occurred:', err);
+         })
+         .on('response', function(result) {
+          console.log(result);
+         })
+         .on('end', function() {
+          console.log('Completed :D');
+         })
+}
+
+
+getRepoContributors("jquery", "jquery");
