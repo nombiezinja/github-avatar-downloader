5b2fa502cd28b36c40cfbc90dfc805ca31f8928d
Ti Zhang <tizhang8835@gmail.com>
2017-06-06 01:30:03 +0000

    Can now retrieve JSON parsed

--- download_avatar.js
+++ download_avatar.js
@@ -2,19 +2,35 @@ var request = require('request');
 
 console.log('Welcome to GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'Nombiezinja';
+var GITHUB_TOKEN = '7e8cbab3e97fe6f54dfab6d362123fafc3afbfbb';
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
-         .on('error', function(err) {
-          console.log('An error has occurred:', err);
-         })
-         .on('response', function(result) {
-          console.log(result);
-         })
-         .on('end', function() {
-          console.log('Completed :D');
-         })
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+
+  var requestOptions = {
+    headers:{
+      "user-agent":"ti"
+    },
+    url: requestURL
+  }
+
+  request.get(requestOptions, function(err, response, body) {
+    console.log('error', err);
+    console.log('statusCode:', response && response.statusCode);
+    cb(body)
+
+  })
+
+
 }
 
+// function print(output) {
+//   console.log(JSON.parse(output));
+// }
 
-getRepoContributors("jquery", "jquery");
+
+getRepoContributors("jquery", "jquery", function print(output) {
+  console.log(JSON.parse(output));
+});
