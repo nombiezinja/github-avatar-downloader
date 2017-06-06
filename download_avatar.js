c11c85ed13658cbdbfbd7ccc6a417680916c71fc
Ti Zhang <tizhang8835@gmail.com>
2017-06-06 02:41:56 +0000

    avatar downloader now takes commandline arguments and returns message if one or both arguments are not entered

--- download_avatar.js
+++ download_avatar.js
@@ -9,6 +9,9 @@ var GITHUB_TOKEN = '7e8cbab3e97fe6f54dfab6d362123fafc3afbfbb';
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  if (!repoOwner || !repoName) {
+    console.log ("Please enter the owner of the repository, then the name of the repository, in that order. ")
+  } else {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
 
   var requestOptions = {
@@ -26,20 +29,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     cb(body);
 
+    console.log('Download complete, please check your directory :)')
+
   }); //request.get
+
+  }
 
 } //function getRepoContributors
 
 
-
-getRepoContributors('jquery', 'jquery', function loop(result) {
+var loop = function(result) {
 
   JSON.parse(result).forEach (function(element) {
       var url = element.avatar_url;
       var filePath = element.login + '.jpg';
-      console.log(' url is ', url);
-      console.log(' file path is ', filePath);
+      console.log('Downloading avatar for ' + element.login);
       downloadIMG(url, filePath);
   });
 
-});
+};
+
+getRepoContributors(process.argv[2], process.argv[3], loop);
